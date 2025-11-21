bbca295ba706c3c76dea5dcaab92505c23774e92
PDJ6975 <antrodcal@alum.us.es>
2025-11-21 20:11:55 +0100

    Initial Commit

---
+++ todo_app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo_app
+//
+//  Created by Antonio Rodríguez Calderón on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
